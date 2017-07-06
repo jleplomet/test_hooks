743199630c1a407c9bc1b7712e775689b0c8bd2b
Server <server@server.com>
2017-07-05 18:15:14 -0700

    i dont know anymore

--- deploy-config.php
+++ deploy-config.php
@@ -44,7 +44,7 @@ define('BRANCH', 'master');
  *
  * @var string Full path including the trailing slash
  */
-define('TARGET_DIR', '/tmp/simple-php-git-deploy/');
+define('TARGET_DIR', '/var/www/vhosts/');
 
 /**
  * Whether to delete the files that are not in the repository but are on the
